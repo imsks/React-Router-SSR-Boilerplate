9570d35befd3fb717fc44569e5e695467fa39d27
Sachin <sachinkshuklaoo7@gmail.com>
2020-11-05 21:56:55 +0530

    added SEO modules

---
+++ src/helpers/renderer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { Helmet } from 'react-helmet';
+import Routes from '../client/Routes';
+import webConfig from '../../webConfig.json';
+
+export default (req, context) => {
+  const content = renderToString(
+    <StaticRouter location={req.path} context={context}>
+      <div>{renderRoutes(Routes)}</div>
+    </StaticRouter>
+  );
+
+  const helmet = Helmet.renderStatic();
+
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta
+        name="googlebot"
+        content="index, follow, max-snippet:-1, max-image-preview:large"
+        />
+        <meta
+          name="bingbot"
+          content="index, follow, max-snippet:-1, max-image-preview:large"
+        />
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
+        <link rel="shortcut icon" href="${webConfig.siteURL}/assets/images/favicon.png">
+    </head>
+            <body>
+                <div id="root">${content}</div>
+                <script src="/bundle.js"></script>
+            </body>
+    </html>`;
+};
---
+++ webpack.client.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const baseConfig = require('./webpack.base');
+const webConfig = require('./webConfig.json');
+
+const config = {
+  mode: 'development',
+  // Tell webpack to root file of our server app
+  entry: ['./src/client/client.js', './src/assets/scss/styles.scss'],
+  // Tell webpack where to put output file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          // 'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true,
+              url: true,
+              root: webConfig.siteURL,
+            },
+          },
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+        // use: [
+        //   {
+        //     loader: 'file-loader',
+        //     options: {
+        //       name: '[name].min.css',
+        //       outputPath: 'assets/css/',
+        //     },
+        //   },
+        //   {
+        //     loader: 'extract-loader',
+        //   },
+        //   // {
+        //   //     loader: 'style-loader'
+        //   // },
+        //   {
+        //     loader: 'css-loader',
+        //     options: {
+        //       minimize: true,
+        //       url: true,
+        //       root: webConfig.siteURL,
+        //     },
+        //   },
+        //   {
+        //     loader: 'sass-loader',
+        //   },
+        // ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'src/assets/images', to: 'assets/images' },
+      { from: 'src/assets/important', to: '' },
+    ]),
+  ],
+  devtool: 'inline-source-map',
+};
+
+module.exports = merge(baseConfig, config);
